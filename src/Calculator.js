fec414c82a59ea166ae6ee27bd28e40a06828218
afif95 <afifbinkamrul@gmail.com>
2023-10-16 23:38:37 -0400

    first commit

---
+++ src/Calculator.js
@@ -0,0 +1,132 @@
+import { memo, useEffect, useState } from "react";
+import clickSound from "./ClickSound.m4a";
+
+function Calculator({ workouts, allowSound }) {
+  const [number, setNumber] = useState(workouts.at(0).numExercises);
+  const [sets, setSets] = useState(3);
+  const [speed, setSpeed] = useState(90);
+  const [durationBreak, setDurationBreak] = useState(5);
+
+  const [duration, setDuration] = useState(0);
+
+  // placing this function out of the component or inside useEffect won't work, so memoize
+  // const playSound = useCallback(
+  //   function () {
+  //     if (!allowSound) return;
+  //     const sound = new Audio(clickSound);
+  //     sound.play();
+  //   },
+  //   [allowSound]
+  // );
+
+  // big problem (single useEffect for all states): clicking sound button resets the timer back to the original result after increasing or decreasing it by pressing +/-
+  // useEffect creates double render as it runs way after the render has already happened
+  useEffect(
+    function () {
+      setDuration((number * sets * speed) / 60 + (sets - 1) * durationBreak);
+      // playSound();
+    },
+    // [number, sets, speed, durationBreak, playSound]
+    [number, sets, speed, durationBreak]
+  );
+
+  // big problem solution
+  useEffect(
+    function () {
+      const playSound = function () {
+        if (!allowSound) return;
+        const sound = new Audio(clickSound);
+        sound.play();
+      };
+      playSound();
+    },
+    // duration
+    [duration, allowSound]
+  );
+
+  // []: the function inside is created in the beginning and never again, therefore, get a closure with the initial snapshot
+  // [number]: the function closes over a new snapshot when number changes, values of duration and sets will be stale values inside a stale closure (outdated values)
+  // [number, duration, sets]: eliminated stale closure
+  useEffect(
+    function () {
+      console.log(duration, sets);
+      document.title = `Your ${number}-exercise workout`;
+    },
+    [number, duration, sets]
+  );
+
+  const mins = Math.floor(duration);
+  const seconds = (duration - mins) * 60;
+
+  function handleInc() {
+    setDuration((duration) => Math.floor(duration) + 1);
+    // playSound();
+  }
+
+  function handleDec() {
+    setDuration((duration) => (duration > 1 ? Math.ceil(duration) - 1 : 0));
+    // playSound();
+  }
+
+  return (
+    <>
+      <form>
+        <div>
+          <label>Type of workout</label>
+          <select value={number} onChange={(e) => setNumber(+e.target.value)}>
+            {workouts.map((workout) => (
+              <option value={workout.numExercises} key={workout.name}>
+                {workout.name} ({workout.numExercises} exercises)
+              </option>
+            ))}
+          </select>
+        </div>
+        <div>
+          <label>How many sets?</label>
+          <input
+            type="range"
+            min="1"
+            max="5"
+            value={sets}
+            onChange={(e) => setSets(e.target.value)}
+          />
+          <span>{sets}</span>
+        </div>
+        <div>
+          <label>How fast are you?</label>
+          <input
+            type="range"
+            min="30"
+            max="180"
+            step="30"
+            value={speed}
+            onChange={(e) => setSpeed(e.target.value)}
+          />
+          <span>{speed} sec/exercise</span>
+        </div>
+        <div>
+          <label>Break length</label>
+          <input
+            type="range"
+            min="1"
+            max="10"
+            value={durationBreak}
+            onChange={(e) => setDurationBreak(e.target.value)}
+          />
+          <span>{durationBreak} minutes/break</span>
+        </div>
+      </form>
+      <section>
+        <button onClick={handleDec}>–</button>
+        <p>
+          {mins < 10 && "0"}
+          {mins}:{seconds < 10 && "0"}
+          {seconds}
+        </p>
+        <button onClick={handleInc}>+</button>
+      </section>
+    </>
+  );
+}
+
+export default memo(Calculator);
